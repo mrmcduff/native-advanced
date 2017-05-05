02308c8680b1bbe8abe6f3bef9caa78d78b5fce6
Michael McDuffee <mrmcduff@stripe.com>
2017-05-05 15:26:33 -0700

    removing the sample ball animation

--- swipe/src/Ball.js
+++ swipe/src/Ball.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 
 class Ball extends Component {
+
+  componentWillMount() {
+    this.position = new Animated.ValueXY(0, 0);
+    Animated.spring(this.position, {
+      toValue: { x: 200, y: 500 }
+    }).start();
+  }
+
   render() {
+    console.log(this.position.getLayout());
     return (
-      <View style={styles.ball} />
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
     );
   }
 }
@@ -15,7 +26,7 @@ const styles = {
     width: 60,
     borderRadius: 30,
     borderWidth: 30,
-    borderColor: 'black'
+    borderColor: 'red'
   }
 }
 
